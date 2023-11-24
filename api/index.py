583e22c7d307f7eb904f07b380c482e8340e1a11
Prajyu Ms <prajyu2003@gmail.com>
2023-11-24 17:19:25 +0530

    UI width fix

--- api/index.py
+++ api/index.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+import os
+from typing import Optional, List
+
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import Response
+from pydantic import ConfigDict, BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
+
+from typing_extensions import Annotated
+
+from bson import ObjectId
+import motor.motor_asyncio
+from pymongo import ReturnDocument
 
 app = FastAPI()
 
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/studenthub")
+db = client["studenthubconnect"]
+student_collection = db.get_collection("studenthub")
+
 @app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+def home():
+    return {"message": "asd"}
