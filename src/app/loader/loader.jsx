7089743a0f1dbeaacb7d9f8750a393ebfb118f7c
harinarayanan-kp <harikpaug18@gmail.com>
2023-11-25 03:27:30 +0530

    Loader

---
+++ src/app/loader/loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div style={{height: "100vh"}} className='center'>
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
