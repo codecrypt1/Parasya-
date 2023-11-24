3f73a6ba0339d5625d48010538a399cbd5380e1c
Prajyu Ms <prajyu2003@gmail.com>
2023-11-25 01:21:34 +0530

    Database error

--- api/index.py
+++ api/index.py
@@ -1,59 +1,68 @@
-import os
-from typing import Optional, List
-
-from fastapi import FastAPI, Body, HTTPException, status, Depends
-from fastapi.responses import Response
-from pydantic import ConfigDict, BaseModel, Field, EmailStr
-from pydantic.functional_validators import BeforeValidator
-from fastapi.security import OAuth2PasswordBearer
-import requests
-from jose import jwt
-
-from typing_extensions import Annotated
-
-from bson import ObjectId
-import motor.motor_asyncio
-from pymongo import ReturnDocument
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-GOOGLE_CLIENT_ID = "1000665711129-8m8dnlj3u4jrvoo21a9hpeaq46tu4idf.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
-GOOGLE_REDIRECT_URI = "http://localhost:3000/"
-
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
 
 app = FastAPI()
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/studenthub")
-db = client["studenthubconnect"]
-student_collection = db.get_collection("studenthub")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/api/login/google")
-async def login_google():
-    return {
-        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
-    }
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient("mongodb://localhost:27017")
+    app.mongodb = app.mongodb_client["studenthub"]
+    app.collection = app.mongodb["users"]
 
-@app.get("/api/auth/google")
-async def auth_google(code: str):
-    token_url = "https://accounts.google.com/o/oauth2/token"
-    data = {
-        "code": code,
-        "client_id": GOOGLE_CLIENT_ID,
-        "client_secret": GOOGLE_CLIENT_SECRET,
-        "redirect_uri": GOOGLE_REDIRECT_URI,
-        "grant_type": "authorization_code",
-    }
-    response = requests.post(token_url, data=data)
-    access_token = response.json().get("access_token")
-    user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
-    return user_info.json()
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
 
-@app.get("/token")
-async def get_token(token: str = Depends(oauth2_scheme)):
-    return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
+@app.get("/api")
+async def read_root():
+    return {"message": "Hello, FastAPI!"}
 
-@app.get("/api/python")
-def home():
-    return {"message": "asd"}
+class User(BaseModel):
+    name: str
+    email:str
+    password:str
+    college:str
+
+class Forum(BaseModel):
+    name:str
+    image:str
+    title:str
+    context:str
+
+@app.post("/api/items/")
+async def create_item(item: User):
+    inserted_item = await app.collection.insert_one(item.dict())
+    return {"id": str(inserted_item.inserted_id)}
+
+
+@app.get("/api/items/{item_id}")
+async def read_item(item_id: str):
+    document = await app.collection.find_one({"_id": item_id})
+    if document:
+        return document
+    raise HTTPException(status_code=404, detail="Item not found")
+
+@app.put("/api/items/{item_id}")
+async def update_item(item_id: str, item: User):
+    updated_item = await app.collection.update_one(
+        {"_id": item_id}, {"$set": item.dict()}
+    )
+    if updated_item.modified_count:
+        return {"message": "Item updated successfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
+@app.delete("/api/items/{item_id}")
+async def delete_item(item_id: str):
+    deleted_item = await app.collection.delete_one({"_id": item_id})
+    if deleted_item.deleted_count:
+        return {"message": "Item deleted successfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,41 +1,9 @@
-"use client";
-
-import React from "react";
-import { useEffect } from "react";
-import { gapi } from "gapi-script";
-import Login from "./login";
-import Logout from "./logout";
-
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
-
-const clientID =
-  "746083793683-k3116klr7ft06jdcmiupfonlg3pi6ucj.apps.googleusercontent.com";
-
-const LoginPage = () => {
-  const responseMessage = (response) => {
-    console.log(response);
-  };
-  const errorMessage = (error) => {
-    console.log(error);
-  };
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientID: clientID,
-        scope: "",
-      });
-    }
-    gapi.load("client:auth2", start);
-  });
+const Login = () => {
   return (
     <div>
-      <GoogleOAuthProvider clientId={clientID}>
-        LOGIN/LOGOUT
-        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-        {/* <Logout /> */}
-      </GoogleOAuthProvider>
+      <div>Login</div>
     </div>
   );
 };
 
-export default LoginPage;
+export default Login;
