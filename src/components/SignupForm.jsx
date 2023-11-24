5081db18bff504859393982974cd7120712e13d4
Vishnu-dk <vishnudk48@gmail.com>
2023-11-25 02:04:39 +0530

    c changed x

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -112,8 +112,8 @@ const SignupForm = () => {
                     Your College Id
                   </label>
                   <input
-                    type="tect"
-                    name="college id"
+                    type="text"
+                    name="college_id"
                     id="id"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cayn-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500"
                     placeholder="22B857"
