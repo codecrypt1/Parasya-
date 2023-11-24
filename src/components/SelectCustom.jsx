4e47e99a82a9558ab041c96b3510f97467e39408
Prajyu Ms <prajyu2003@gmail.com>
2023-11-24 17:21:14 +0530

    Dynamic component

--- src/components/SelectCustom.jsx
+++ src/components/SelectCustom.jsx
@@ -3,10 +3,10 @@
 import React from "react";
 import { Select, SelectItem } from "@nextui-org/react";
 
-const SelectCustom = ({ options }) => {
+const SelectCustom = ({ options, label }) => {
   return (
     <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-      <Select label="Select a College" className="">
+      <Select label={label} className="">
         {options.map((e) => (
           <SelectItem key={e.value} value={e.value}>
             {e.label}
