fda4a1af2edd0cca2767da63e3dd9a30de3bdf46
Abhay Sunil <abhaysunil2002@gmail.com>
2023-11-24 14:26:46 +0530

    home main

---
+++ src/components/HomeMain.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomeMain = () => {
+  return (
+    <div className="bg-cyan-950 flex items-center mx-auto w-[100%] h-[100%]">
+      <h1 className="text-gray-300 text-left text-6xl mx-auto  gap-2">
+        Empowering Students <br />{" "}
+        <span className="my-2">Simplifying Rentals </span> <br />{" "}
+        <span className="my-2"> Enriching Lives </span>{" "}
+      </h1>
+    </div>
+  );
+};
+export default HomeMain;
